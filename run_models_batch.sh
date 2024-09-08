ff5ea5390428010920b90efbb1d570aaf2708046
JALAL <enggjalal111@gmail.com>
2024-09-08 16:18:48 +0500

    Add files via upload

---
+++ run_models_batch.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# */AIPND-revision/intropyproject-classify-pet-images/run_models_batch.sh
+#                                                                             
+# PROGRAMMER: Jalal
+# DATE CREATED: 15 DEC 2022                                 
+# REVISED DATE: 17 DEC 2022  - 
+# PURPOSE: Runs all three models to test which provides 'best' solution.
+#          Please note output from each run has been piped into a text file.
+#
+# Usage: sh run_models_batch.sh    -- will run program from commandline within Project Workspace
+#  
+python check_images.py --dir pet_images/ --arch resnet  --dogfile dognames.txt > resnet_pet-images.txt
+python check_images.py --dir pet_images/ --arch alexnet --dogfile dognames.txt > alexnet_pet-images.txt
+python check_images.py --dir pet_images/ --arch vgg  --dogfile dognames.txt > vgg_pet-images.txt
